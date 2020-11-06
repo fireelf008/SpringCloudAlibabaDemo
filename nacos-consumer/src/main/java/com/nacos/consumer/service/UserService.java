120dd706cf7d41dfa4d6d10e2dfb6aaebe9f81ab
wangshaofu <fireelf008@163.com>
2020-11-06 18:25:21 +0800

    no message

--- nacos-consumer/src/main/java/com/nacos/consumer/service/UserService.java
+++ nacos-consumer/src/main/java/com/nacos/consumer/service/UserService.java
@@ -2,12 +2,15 @@ package com.nacos.consumer.service;
 
 import com.nacos.consumer.entity.User;
 import com.nacos.consumer.mapper.UserMapper;
+import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -18,11 +21,13 @@ public class UserService {
 
     @GlobalTransactional
     public String updateUser() {
+        log.info("事务ID:" + RootContext.getXID());
         User user = this.userMapper.selectById(1285052338640195585L);
         user.setAge(30);
-        int i = 1/0;
         this.userMapper.updateById(user);
+        int i = 1/0;
         String reuslt = restTemplate.getForObject("http://nacos-producer/nacos/producer/producer",String.class);
+
         return reuslt;
     }
 }
--- nacos-producer/src/main/java/com/nacos/producer/service/UserService.java
+++ nacos-producer/src/main/java/com/nacos/producer/service/UserService.java
@@ -2,11 +2,14 @@ package com.nacos.producer.service;
 
 import com.nacos.producer.entity.User;
 import com.nacos.producer.mapper.UserMapper;
+import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -14,10 +17,11 @@ public class UserService {
 
     @GlobalTransactional
     public User updateUser() {
+        log.info("事务ID:" + RootContext.getXID());
         User user = this.userMapper.selectById(1285052338640195585L);
         user.setAge(20);
         this.userMapper.updateById(user);
-        int i = 1/0;
+//        int i = 1/0;
         return user;
     }
 }
